3468bb75365f32d40770eb0d653b44caf3194a42
Michał Zawada <m.zawada@controlexpert.com>
2024-12-26 20:21:14 +0100

    add win module

--- wezterm.lua
+++ wezterm.lua
@@ -2,8 +2,10 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 local config = wezterm.config_builder()
 local platform = wezterm.target_triple
+local is_win = platform == "x86_64-pc-windows-msvc"
 
-if platform == "x86_64-pc-windows-msvc" then
+if is_win then
+	require("win")
 	config.default_prog = {
 		"C:\\Program Files\\PowerShell\\7\\pwsh.exe",
 		"-NoLogo",
@@ -12,6 +14,7 @@ if platform == "x86_64-pc-windows-msvc" then
 	}
 end
 
+-- debug
 wezterm.on("update-right-status", function(window)
 	local tab = window:active_tab()
 	if tab == nil then
@@ -59,7 +62,7 @@ config.colors = {
 		},
 		active_tab = {
 			-- underline = "Single",
-			bg_color = "#0C0C0C",
+			bg_color = "#121212",
 			fg_color = "#FFFFFF",
 		},
 		new_tab_hover = {
@@ -145,60 +148,12 @@ wezterm.on("update-right-status", function(window, pane)
 	-- Time
 	-- local time = wezterm.strftime("%H:%M")
 
-	-- Let's add color to one of the components
 	window:set_right_status(wezterm.format({
 		{ Text = wezterm.nerdfonts.oct_table .. " " .. stat },
 		-- { Text = " | " },
 		-- { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
 		-- { Text = " |" },
 	}))
-end)
-
-local basename = function(s)
-	return string.gsub(s, "(.*[/\\])(.*)", "%2")
-end
-
-local tab_title = function(tab_info)
-	local active_pane = tab_info.active_pane
-	local current_dir = basename(tostring(active_pane.current_working_dir))
-
-	local active_procces = active_pane.foreground_process_name
-	local tab_title = string.lower(active_pane.title)
-	local tab_index = tostring(tab_info.tab_index + 1)
-	local icon = wezterm.nerdfonts.cod_terminal
-	local text_color = "White"
-
-	if string.find(tab_title, "nvim") then
-		icon = wezterm.nerdfonts.custom_neovim
-		text_color = "#4ADE80"
-	elseif string.find(tab_title, "lazygit") then
-		icon = wezterm.nerdfonts.fa_git
-		text_color = "#F05033"
-	elseif string.find(active_procces, "node") then
-		icon = wezterm.nerdfonts.dev_nodejs_small
-		text_color = "#43853D"
-	elseif string.find(active_procces, "net") then
-		icon = wezterm.nerdfonts.md_dot_net
-		text_color = "#512BD4"
-	end
-
-	if tab_index and #tab_index > 0 then
-		return " " .. tab_index .. ": " .. current_dir .. " " .. icon .. " ", text_color
-	end
-
-	return tab_info.active_pane.title
-end
-
-wezterm.on("format-tab-title", function(tab, _, _, _, _, _)
-	local title, text_color = tab_title(tab)
-	if tab.is_active then
-		return {
-			-- { Background = { Color = "866CBA" } },
-			{ Foreground = { Color = text_color } },
-			{ Text = title },
-		}
-	end
-	return title
 end)
 
 return config
---
+++ win.lua
@@ -0,0 +1,48 @@
+local wezterm = require("wezterm")
+
+local basename = function(s)
+	return string.gsub(s, "(.*[/\\])(.*)", "%2")
+end
+
+local tab_title = function(tab_info)
+	local active_pane = tab_info.active_pane
+	local current_dir = basename(tostring(active_pane.current_working_dir))
+
+	local active_procces = active_pane.foreground_process_name
+	local tab_title = string.lower(active_pane.title)
+	local tab_index = tostring(tab_info.tab_index + 1)
+	local icon = wezterm.nerdfonts.cod_terminal
+	local text_color = "White"
+
+	if string.find(tab_title, "nvim") then
+		icon = wezterm.nerdfonts.custom_neovim
+		text_color = "#4ADE80"
+	elseif string.find(tab_title, "lazygit") then
+		icon = wezterm.nerdfonts.fa_git
+		text_color = "#F05033"
+	elseif string.find(active_procces, "node") then
+		icon = wezterm.nerdfonts.dev_nodejs_small
+		text_color = "#43853D"
+	elseif string.find(active_procces, "net") then
+		icon = wezterm.nerdfonts.md_dot_net
+		text_color = "#512BD4"
+	end
+
+	if tab_index and #tab_index > 0 then
+		return " " .. tab_index .. ": " .. current_dir .. " " .. icon .. " ", text_color
+	end
+
+	return tab_info.active_pane.title
+end
+
+wezterm.on("format-tab-title", function(tab, _, _, _, _, _)
+	local title, text_color = tab_title(tab)
+	if tab.is_active then
+		return {
+			-- { Background = { Color = "866CBA" } },
+			{ Foreground = { Color = text_color } },
+			{ Text = title },
+		}
+	end
+	return title
+end)
